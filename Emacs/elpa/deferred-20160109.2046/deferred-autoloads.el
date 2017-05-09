0ab9eb94f656de72a5f1f6911de9c481e2227182
hylkema <jouke.hijlkema@onera.fr>
2017-05-09 14:42:31 +0200

    added auto-complete

--- Emacs/elpa/deferred-20160109.2046/deferred-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; deferred-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("deferred.el") (22239 1784 997718 535000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; deferred-autoloads.el ends here
