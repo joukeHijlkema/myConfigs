1149871b74ce568ad681cb2c4a6c2b2a7f2a6318
hylkema <jouke.hijlkema@onera.fr>
2017-05-09 14:52:11 +0200

    added auto-complete

--- Emacs/myInit.el
+++ Emacs/myInit.el
@@ -59,3 +59,6 @@
                ("\\subsubsection{%s}" . "\\subsubsection{%s}")
                ("\\paragraph{%s}" . "\\paragraph{%s}")
                ("\\subparagraph{%s}" . "\\subparagraph{%s}")))
+
+
+(add-to-list 'auto-mode-alist '("\\.py\\'"		.	auto-coplete-mode))
