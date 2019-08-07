ec883467d3ce9108bbb3de7288619da70b5e853b
jouke hijlkema <jouke.hijlkema@gmail.com>
2019-08-07 14:19:11 +0200

    cleanup

--- Emacs/init.el
+++ Emacs/init.el
@@ -14,6 +14,7 @@
 			 ("org" . "https://orgmode.org/elpa/")
 			 )
       )
+
 (package-initialize)
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -21,83 +22,26 @@
 
 (eval-when-compile
   (require 'use-package))
+(setq use-package-verbose 'Debug)
+
+(load "~/.emacs.d/myInit.el")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(TeX-source-correlate-method (quote ((dvi . source-specials) (pdf . synctex))))
- '(TeX-source-correlate-mode t)
- '(TeX-source-correlate-start-server t)
- '(TeX-view-program-selection
-   (quote
-    (((output-dvi has-no-display-manager)
-      "dvi2tty")
-     ((output-dvi style-pstricks)
-      "dvips and gv")
-     (output-dvi "xdvi")
-     (output-pdf "Atril")
-     (output-html "xdg-open"))))
- '(cua-mode t nil (cua-base))
- '(custom-enabled-themes (quote (lab-dark)))
  '(custom-safe-themes
    (quote
-    ("980f0adf3421c25edf7b789a046d542e3b45d001735c87057bccb7a411712d09" "a2afb83e8da1d92f83543967fb75a490674a755440d0ce405cf9d9ae008d0018" "392395ee6e6844aec5a76ca4f5c820b97119ddc5290f4e0f58b38c9748181e8d" "13d20048c12826c7ea636fbe513d6f24c0d43709a761052adbca052708798ce3" default)))
- '(dired-omit-files "^\\.+.+\\|^\\.?#\\|^\\.$\\|^\\.\\.$")
- '(doc-view-continuous t)
+    ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "5e515425f8a5ce00097d707742eb5eee09b27cebc693b8998c734305cbdce1f5" "d14d421ff49120d2c2e0188bcef76008407b3ceff2cfb1d4bdf3684cf3190172" default)))
  '(ecb-options-version "2.50")
- '(ecb-source-path
-   (quote
-    (("/ssh:pi@lp-rpi-cam3:" "/ssh:pi@lp-rpi-cam3:")
-     ("/" "/"))))
- '(ede-project-directories
-   (quote
-    ("/home/hylkema/Programs/rpiCameras" "/home/hylkema/Programs/Tools/Cedre")))
- '(global-linum-mode t)
- '(inhibit-startup-screen t)
- '(initial-frame-alist (quote ((fullscreen . maximized))))
- '(ispell-dictionary "fr")
- '(org-agenda-files
-   (quote
-    ("/home/hylkema/Documents/Org/CNES_RT/2017/CNES_RT_2017.org" "/home/hylkema/Documents/Org/CNES_RT/2018_2019/CNES_RT_2018_2019.org" "/home/hylkema/Documents/Org/ESA/Hydrazine/EsaTrp.org" "/home/hylkema/Documents/Org/journal.org" "/home/hylkema/Documents/Org/Kerc/RG2018.org" "/home/hylkema/Documents/Org/Cours_stages_theses/Theses.org" "/home/hylkema/Documents/Org/Cours_stages_theses/IATOM.org" "/home/hylkema/Documents/Org/Test.org" "/home/hylkema/Documents/Org/Divers/Divers.org" "/home/hylkema/Documents/Org/Divers/CU.org" "/home/hylkema/Documents/Org/Divers/PakOma.org" "/home/hylkema/Documents/Org/Divers/cheatSheet.org" "/home/hylkema/Documents/Org/Divers/SP18.org" "/home/hylkema/Documents/Org/Reviews/AA_2018_170.org" "/home/hylkema/Documents/Org/gtd.org" "/home/hylkema/Documents/Org/Perso/applecross.org" "/home/hylkema/Documents/Org/Perso/changementBanque.org" "/home/hylkema/Documents/Org/Perso/Vlieland.org" "/home/hylkema/Documents/Org/Perso/Toit.org" "/home/hylkema/Documents/Org/Perso/Perso.org" "/home/hylkema/Documents/Org/Perseus/Perseus.org")))
- '(org-icalendar-combined-description "Org mode calendar")
- '(org-icalendar-timezone nil)
- '(org-icalendar-use-deadline (quote (event-if-not-todo event-if-todo todo-due)))
- '(org-icalendar-with-timestamps t)
  '(package-selected-packages
    (quote
-    (dired-subtree lab-themes adaptive-wrap neotree treemacs imenu-list org-kanban magit-annex magit-filenotify magit-find-file magit-gerrit magit-gh-pulls magit-gitflow magit-imerge magit-lfs magit-org-todos magit-popup magithub calfw-cal counsel-org-capture-string spacemacs-theme flatui-theme moe-theme org jedi realgud flycheck-gradle gradle-mode elpy sunrise-x-w32-addons mc-extras latex-preview-pane latex-pretty-symbols counsel swiper openwith sunrise-commander sunrise-x-buttons sunrise-x-checkpoints sunrise-x-loop sunrise-x-mirror sunrise-x-modeline sunrise-x-popviewer sunrise-x-tabs sunrise-x-tree org-plus-contrib yasnippet-snippets ac-math auto-complete-auctex langtool calfw-org calfw-ical calfw use-package mu4e-alert mu4e-maildirs-extension mu4e-query-fragments w3m wanderlust org-journal markdown-preview-eww markdown-mode ibuffer-projectile ibuffer-vc projectile-codesearch projectile-git-autofetch projectile-speedbar zephir-mode projectile dh-elpa wsd-mode web-mode virtualenv uncru stify-mode tabbar-ruler sr-speedbar plantuml-mode php+-mode multiple-cursors minimap magit highlight-indentation gnuplot-mode gnuplot flycheck find-file-in-project epc egg ecb direx com pany-irony-c-headers company-irony clang-format auto-install auctex-lua auctex-latexmk)))
- '(quote (cua-mode t nil (cua-base)))
- '(reftex-toc-split-windows-horizontally t)
- '(safe-local-variable-values
-   (quote
-    ((TeX-command-extra-options . "-shell-escape")
-     (eval let nil
-	   (save-excursion
-	     (org-babel-goto-named-src-block "startblock")
-	     (org-babel-execute-src-block))))))
- '(show-paren-mode t)
- '(sr-speedbar-right-side nil)
- '(sr-speedbar-skip-other-window-p t)
- '(tool-bar-mode nil)
- '(virtualenv-root "~hylkema/")
- '(wg-emacs-exit-save-behavior (quote nill))
- '(wg-morph-on nil))
-
-(put 'upcase-region 'disabled nil)
-
-;; load calendar at start
-;; (calendar)
-;;=======
-(put 'downcase-region 'disabled nil)
-
-(load "~/.emacs.d/myInit.el")
-
-=======
+    (magit spacemacs-themes company-mode auto-complete-config cua-mode t: android-env workgroups2 web-mode use-package uncrustify-mode tabbar sunrise-x-buttons sr-speedbar spacemacs-theme semi org-plus-contrib org-journal org-edit-latex org openwith multiple-cursors markdown-mode langtool lab-themes hydra elpy ecb dired-explorer calfw-org calfw arduino-mode ac-math)))
+ '(python-shell-interpreter "python3" t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(mu4e-unread-face ((t (:inherit font-lock-keyword-face :foreground "dark orange" :weight bold)))))
+ )
--- Emacs/myInit.el
+++ Emacs/myInit.el
@@ -1,35 +1,45 @@
-;; remove toolbar
+;; === THIS STILL NEEDS TO BE MOVED TO use-package ===
+;; (add-to-list 'auto-mode-alist '("\\.hpp\\'" . c++-mode))
+;; (add-to-list 'auto-mode-alist '("\\.cpp\\'" . c++-mode))
+;; (add-to-list 'auto-mode-alist '("\\.FCMacro\\'" . python-mode))
+
+
+;; === Yas ===
+(load "~/.emacs.d/myConfig/myYas.el")
+;; === Web mode ===
+(load "~/.emacs.d/myConfig/myWeb.el")
+;; === Workgroups ===
+(load "~/.emacs.d/myConfig/myWg.el")
+;; === line numbers off for certain modes ===
+(load "~/.emacs.d/myConfig/myLinum-off.el")
+;; === langtool ===
+(load "~/.emacs.d/myConfig/myLangtool.el")
+;; === autoComplete ===
+(load "~/.emacs.d/myConfig/myAutoComplete.el")
+;; === Latex  ===
+(load "~/.emacs.d/myConfig/myLatex.el")
+;; === Org mode ===
+(load "~/.emacs.d/myConfig/myOrg.el")
+;; === mail ===
+(when (string= system-name "LDMPE705H") (load "~/.emacs.d/mu4e.el"))
+(when (string= system-name "LDMPE709H") (load "~/.emacs.d/mu4e.el"))
+;; === keys ===
+(load "~/.emacs.d/myConfig/myKeys.el")
+;; === Dired ===
+(load "~/.emacs.d/myConfig/myDired.el")
+;; === Open with ===
+(load "~/.emacs.d/myConfig/myOpenWith.el")
+;; === python ===
+(load "~/.emacs.d/myConfig/myPython.el")
+;; === Java ===
+;; (load "~/.emacs.d/java.el")
+;; === Android development ===
+(load "~/.emacs.d/myConfig/myAndroid.el")
+
+;; some global stuff
 (tool-bar-mode -1)
-
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
-(setq ivy-count-format "(%d/%d) ")
-
-(add-to-list 'auto-mode-alist '("\\.hpp\\'" . c++-mode))
-(add-to-list 'auto-mode-alist '("\\.cpp\\'" . c++-mode))
-(add-to-list 'auto-mode-alist '("\\.FCMacro\\'" . python-mode))
-
-;; (tabbar-mode t)
-(yas-global-mode t)
 (setq tramp-default-method "ssh")
-(setq speedbar-initial-expansion-list-name "buffers")
-
-(autoload 'cflow-mode "cflow-mode")
-(setq auto-mode-alist (append auto-mode-alist '(("\\.cflow$" . cflow-mode))))
-
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.phtml\\'"		.	web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'"	.	web-mode))
-(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'"		.	web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'"		.	web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'"		.	web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'"	.	web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'"		.	web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?\\'"		.	web-mode))
-(add-to-list 'auto-mode-alist '("\\.css?\\'"		.	web-mode))
-(add-to-list 'auto-mode-alist '("\\.xml?\\'"		.	web-mode))
-(add-to-list 'auto-mode-alist '("\\.php\\'"		.	web-mode))
-
+(setq browse-url-browser-function 'browse-url-default-browser)
 
 (add-hook 'ibuffer-hook
     (lambda ()
@@ -37,83 +47,13 @@
       (unless (eq ibuffer-sorting-mode 'alphabetic)
         (ibuffer-do-sort-by-alphabetic))))
 
-(require 'use-package)
-;; Workgroups2
-(require 'workgroups2)
-(workgroups-mode 1)
-(setq wg-emacs-exit-save-behavior           'save)      ; Options: 'save 'ask nil
-(setq wg-workgroups-mode-exit-save-behavior 'save)      ; Options: 'save 'ask nil
-
-;; === firefox as browser ===
-(setq browse-url-browser-function 'browse-url-firefox)
-
-;; === line numbers off for certain modes ===
-(load "~/.emacs.d/linum-off.el")
-;; === langtool ===
-<<<<<<< HEAD
-(require 'langtool)
-(setq langtool-language-tool-jar "/Software/LanguageTool-4.0/languagetool-commandline.jar")
-(setq langtool-default-language "fr")
-(global-set-key (kbd "s-g") 'langtool-check)
-(global-set-key (kbd "C-s-g") 'langtool-check-done)
-
-=======
-(load "~/.emacs.d/langtool.el")
-;; === autoComplete ===
-(load "~/.emacs.d/autoComplete.el")
->>>>>>> df545d42a11bdec2d629cc9b3f43b0e9c6e6b02a
-;; === Latex  ===
-(load "~/.emacs.d/Latex.el")
-;; === Org mode ===
-(load "~/.emacs.d/momMode.el")
-<<<<<<< HEAD
-;; === mu4e ===
-(when (string= system-name "your.ubuntu.host") (load "~/.emacs.d/mu4e.el") )
-=======
-;; === mail ===
-(when (string= system-name "LDMPE705H") (load "~/.emacs.d/mu4e.el"))
-(when (string= system-name "LDMPE709H") (load "~/.emacs.d/mu4e.el"))
-;; (load "~/.emacs.d/nevermore.el")
-;;(load "~/.emacs.d/notmuch.el")
->>>>>>> df545d42a11bdec2d629cc9b3f43b0e9c6e6b02a
-;; === keys ===
-(load "~/.emacs.d/keys.el")
-;; === sunrine ===
-;; (load "~/.emacs.d/mySunrise.el")
-;; === Dired ===
-(load "~/.emacs.d/Dired.el")
-;; === Open with ===
-(load "~/.emacs.d/openWith.el")
-;; (load "~/.emacs.d/myTheme.el")
-;; === java ===
-;; (load "~/.emacs.d/java.el")
-;; === python ===
-(load "~/.emacs.d/python.el")
-
-;;; auto-complete setup, sequence is important
-(require 'auto-complete)
-(add-to-list 'ac-modes 'latex-mode) ; beware of using 'LaTeX-mode instead
-(require 'ac-math) ; package should be installed first 
-(defun my-ac-latex-mode () ; add ac-sources for latex
-   (setq ac-sources
-         (append '(ac-source-math-unicode
-           ac-source-math-latex
-           ac-source-latex-commands)
-                 ac-sources)))
-(add-hook 'LaTeX-mode-hook 'my-ac-latex-mode)
-(setq ac-math-unicode-in-math-p t)
-(ac-flyspell-workaround)          ; fixes a known bug of delay due to flyspell (if it is there)
-(add-to-list 'ac-modes 'org-mode) ; auto-complete for org-mode (optional)
-(require 'auto-complete-config)   ; should be after add-to-list 'ac-modes and hooks
-(ac-config-default)
-(setq ac-auto-start nil)            ; if t starts ac at startup automatically
-(setq ac-auto-show-menu t)
-(global-auto-complete-mode t) 
-
 (setq set-language-environment "utf-8")
 (define-coding-system-alias 'UTF-8 'utf-8)
 (set-default-coding-systems 'utf-8)
 
 (setq ecb-tip-of-the-day nil)
-(custom-set-variables '(ecb-options-version "2.50")) 
+(custom-set-variables '(ecb-options-version "2.50"))
 
+(show-paren-mode 1)
+
+(server-start)
